c8fe8c7e8d961072228eea05b50d86275eab2ff9
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-26 12:17:14 -0300

    chore: initial project setup.

---
+++ src/main/java/br/com/bancox_security_jwt/BancoxSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.bancox_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoxSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoxSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bancox_security_jwt/BancoxSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bancox_security_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoxSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
