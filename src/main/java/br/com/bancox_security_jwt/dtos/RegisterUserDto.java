2308b6bd40d1ce649df880431936f4f0b182f72c
Joaoapbrito1 <joao.brito@zup.com.br>
2025-02-26 20:10:11 -0300

    refactor: adjusts code of RegisterUserDto, SecurityConfig and UserServiceImpl classes.

--- src/main/java/br/com/bancox_security_jwt/dtos/RegisterUserDto.java
+++ src/main/java/br/com/bancox_security_jwt/dtos/RegisterUserDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Set;
-
+@Setter
 @Getter
 @Data
 public class RegisterUserDto {
@@ -13,8 +13,6 @@ public class RegisterUserDto {
     private String username;
     private String password;
     private Set<Roles> roles;
-    @Setter
     private String department;
-
 
 }
--- src/main/java/br/com/bancox_security_jwt/services/UserServiceImpl.java
+++ src/main/java/br/com/bancox_security_jwt/services/UserServiceImpl.java
@@ -7,36 +7,31 @@ import br.com.bancox_security_jwt.models.UserModel;
 import br.com.bancox_security_jwt.repository.DepartmentRepository;
 import br.com.bancox_security_jwt.repository.RoleRepository;
 import br.com.bancox_security_jwt.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 import java.util.stream.Collectors;
-
 @Service
-public class UserServiceImpl implements UserService{
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private RoleRepository roleRepository;
-    @Autowired
-    private DepartmentRepository departmentRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
 
-    public void registerUser(RegisterUserDto registerUserDto){
-        if (userRepository.existsByUsername(registerUserDto.getUsername())){
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final DepartmentRepository departmentRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registerUser(RegisterUserDto registerUserDto) {
+        if (userRepository.existsByUsername(registerUserDto.getUsername())) {
             throw new RuntimeException("Unprocess Entity");
         }
 
         UserModel user = new UserModel();
         user.setUsername(registerUserDto.getUsername());
-        user.setPassword(bCryptPasswordEncoder.encode(registerUserDto.getPassword()));
+        user.setPassword(passwordEncoder.encode(registerUserDto.getPassword()));
 
-        Set<RoleModel> roles = registerUserDto
-                .getRoles()
-                .stream()
+        Set<RoleModel> roles = registerUserDto.getRoles().stream()
                 .map(r -> new RoleModel(r.name()))
                 .collect(Collectors.toSet());
         roleRepository.saveAll(roles);
@@ -45,13 +40,10 @@ public class UserServiceImpl implements UserService{
 
         DepartmentModel departmentModel = departmentRepository
                 .findByName(registerUserDto.getDepartment())
-                .orElseGet(() -> {
-                    DepartmentModel newDepartmentModel = new DepartmentModel(registerUserDto.getDepartment());
-                    return departmentRepository.save(newDepartmentModel);
-                });
+                .orElseGet(() -> departmentRepository.save(new DepartmentModel(registerUserDto.getDepartment())));
+
         user.setDepartmentModel(departmentModel);
 
         userRepository.save(user);
-
     }
 }
